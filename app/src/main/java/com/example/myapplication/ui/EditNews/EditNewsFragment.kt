cbc5dac91eda0f74b5fc8d75950a3a5962ded994
charlie <heleyang0311@163.com>
2023-07-28 13:26:17 +0800

    feature: 新闻编辑器图片选择

--- app/src/main/java/com/example/myapplication/ui/EditNews/EditNewsFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/EditNews/EditNewsFragment.kt
@@ -1,24 +1,60 @@
 package com.example.myapplication.ui.EditNews
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.example.myapplication.databinding.FragmentEditNewsBinding
+import com.example.myapplication.utils.requets_codes
 
-class EditNewsFragment:Fragment() {
+class EditNewsFragment : Fragment() {
     private var _binding: FragmentEditNewsBinding? = null
-    val binding  get() = _binding!!
+    val binding get() = _binding!!
 
+    private lateinit var imagePickerLaunchers: Array<ActivityResultLauncher<String>>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        imagePickerLaunchers = Array(2) { index ->
+            registerForActivityResult(
+                ActivityResultContracts.GetContent()
+            ) { uri: Uri? ->
+                uri?.let {
+                    if (index == 0) {
+                        Glide.with(this)
+                            .load(uri)
+                            .into(binding.imageButton)
+                    } else if (index == 1) {
+                        Glide.with(this)
+                            .load(uri)
+                            .into(binding.imageButton2)
+                    }
+                }
+            }
+        }
+    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentEditNewsBinding.inflate(inflater,container,false)
+        _binding = FragmentEditNewsBinding.inflate(inflater, container, false)
 
+        binding.imageButton.setOnClickListener {
+           imagePickerLaunchers[0].launch("image/*")
+        }
+        binding.imageButton2.setOnClickListener {
+            imagePickerLaunchers[1].launch("image/*")
+        }
         return binding.root
     }
 }
---
+++ app/src/main/java/com/example/myapplication/utils/requets_codes.kt
@@ -0,0 +1,8 @@
+package com.example.myapplication.utils
+
+class requets_codes {
+    companion object{
+        const val PICK_SINGLE_IMAGE_REQUEST_CODE = 1000
+        const val PICK_MULTI_IMAGE_REQUEST_CODE = 1001
+    }
+}
